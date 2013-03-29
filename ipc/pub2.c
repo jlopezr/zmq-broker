348ca00ef55cc4f85eedb4be25a6a97dceb28907
Juan López <j.lopez.r@gmail.com>
2013-03-29 09:43:38 -0400

    Push-Pull and Beacon example Testing two publishers to the same topic

---
+++ ipc/pub2.c
@@ -0,0 +1,41 @@
+//  Pathological publisher
+//  Sends out 1,000 topics and then one random update per second
+
+#include "czmq.h"
+
+int main (int argc, char *argv [])
+{
+    zctx_t *context = zctx_new ();
+    void *publisher = zsocket_new (context, ZMQ_PUB);
+     if (argc < 3) {
+       printf("usage: %s <subscription> [--broker|--local|<zmq port>]\r\n",argv[0]);
+       return 1;
+    }
+    if (strcmp(argv[2], "--broker") == 0) {
+	printf("Connecting to ipc://frontend (BROKER)");
+	zsocket_connect (publisher, "ipc://frontend");
+
+    } else if(strcmp(argv[2], "--local") == 0) {
+	printf("Listening on ipc://test (LOCAL)");
+	zsocket_bind (publisher, "ipc://test");
+    } else {
+	printf("Listening on %s\r\n", argv[2]);
+	int rc = zsocket_bind (publisher, argv[2]);
+    	assert(rc==0);
+    }
+
+    //  Ensure subscriber connection has time to complete
+    sleep (1);
+
+    //  Send one random update per second
+    srandom ((unsigned) time (NULL));
+    while (!zctx_interrupted) {
+        sleep (1);
+        zstr_sendm (publisher, argv[1]);
+        int n = randof(1000);
+	printf("Sending %03d\r\n", n);
+	zstr_send (publisher, "A2 VALUE = %03d", n);
+    }
+    zctx_destroy (&context);
+    return 0;
+}
---
+++ tcp/pub2.c
@@ -0,0 +1,39 @@
+//  Pathological publisher
+//  Sends out 1,000 topics and then one random update per second
+
+#include "czmq.h"
+
+int main (int argc, char *argv [])
+{
+    zctx_t *context = zctx_new ();
+    void *publisher = zsocket_new (context, ZMQ_PUB);
+     if (argc < 3) {
+       printf("usage: %s <subscription> [--broker|--local|<zmq port>]\r\n",argv[0]);
+       return 1;
+    }
+    if (strcmp(argv[2], "--broker") == 0) {
+	printf("Connecting to tcp://localhost:5557 (BROKER)");
+	zsocket_connect (publisher, "tcp://localhost:5557");
+
+    } else if(strcmp(argv[2], "--local") == 0) {
+	printf("Listening on tcp://*:5556 (LOCAL)");
+	zsocket_bind (publisher, "tcp://*:5556");
+    } else {
+	printf("Connecting to %s\r\n", argv[2]);
+	zsocket_connect (publisher, argv[2]);
+    }
+
+    //  Ensure subscriber connection has time to complete
+    sleep (1);
+
+    //  Send one random update per second
+    srandom ((unsigned) time (NULL));
+    int i = 0;
+    while (!zctx_interrupted) {
+        sleep (1);
+        zstr_sendm (publisher, argv[1]); //TODO is ZMQ_SNDMORE needed?
+        zstr_send (publisher, "VALUE A2 = %03d", i++);
+    }
+    zctx_destroy (&context);
+    return 0;
+}
