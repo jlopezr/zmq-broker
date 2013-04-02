2d41770c722282b8fe2451c463b73e6c991a0a17
Juan López <j.lopez.r@gmail.com>
2013-04-02 15:53:32 -0400

    Discovery protocol only notifies new and lost services

--- beacon/receiver.c
+++ beacon/receiver.c
@@ -1,6 +1,8 @@
 #include <czmq.h>
 #include "constants.h"
 
+zhash_t* services_found;
+
 int process_services(const char *key, void *item, void *argument) {
     //  si el tiempo_actual > tiempo_guardado+3*TIMEOUT
     //     lo elimino de la tabla
@@ -9,23 +11,25 @@ int process_services(const char *key, void *item, void *argument) {
     long* value = (long*)item;
     long* time = (long*)argument;
 
+    //printf("TIME %ld \r\n", *time); 
+    //printf("KEY %s => %ld\r\n", key, *value);
+    
     if(*time>*value+(BEACON_INTERVAL*BEACON_MISSED)) {
-        printf("TIMEOUT!!!!!!!!!!!!!!!!!!!!!!!\r\n");
+        printf("Service %s has dissapeared\r\n", key);
+	zhash_delete(services_found, key);
     }
 
-    printf("TIME %ld \r\n", *time); 
-    printf("KEY %s => %ld\r\n", key, *value);
 
     return 0;
 }
 
-long now = 0;
 
 int main(void) {
+    static long now = 0;
 
     //  Basic test: create a service and announce it
     zctx_t *ctx = zctx_new ();
-    zhash_t* services_found = zhash_new(); 
+    services_found = zhash_new(); 
     
     //  Create beacon to lookup service
     zbeacon_t *client_beacon = zbeacon_new (9999);
@@ -45,17 +49,22 @@ int main(void) {
             int received_port = (zframe_data (content) [0] << 8)
                                 +  zframe_data (content) [1];
 	    char* service_name = ((char*)content)+2; 
-            printf("IP   DETECTED %s\r\n", ipaddress);
-            printf("PORT DETECTED %d\r\n", received_port);
-            printf("SVC  DETECTED %s\r\n", service_name);
+            //printf("IP   DETECTED %s\r\n", ipaddress);
+            //printf("PORT DETECTED %d\r\n", received_port);
+            //printf("SVC  DETECTED %s\r\n", service_name);
  
             //  si ya existe actualizo su tiempo
 	    //  sino la añado
             long* tmp = malloc(sizeof(long));
 	    *tmp = zclock_time();
+	    int num_items = zhash_size(services_found); 
 	    
 	    zhash_update(services_found, service_name, tmp); 
-            
+           
+	    if(zhash_size(services_found)>num_items) {
+                printf("Service %s has been discovered\r\n", service_name); 
+            } 
+
 	    zframe_destroy (&content);
             free (ipaddress);
 	}
