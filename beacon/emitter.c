5202ebda5f08b29d0c45506f890373eaccc84b54
Juan López <j.lopez.r@gmail.com>
2013-04-02 15:22:58 -0400

    Working on discovery protocol

--- beacon/emitter.c
+++ beacon/emitter.c
@@ -1,6 +1,6 @@
 #include <czmq.h>
 
-int main(void) {
+int main(int argc, char** argv) {
 
     //  Basic test: create a service and announce it
     zctx_t *ctx = zctx_new ();
@@ -9,11 +9,27 @@ int main(void) {
     void *service = zsocket_new (ctx, ZMQ_PUB);
     int port_nbr = zsocket_bind (service, "tcp://*:*");
 
+    char* service_name;
+    if(argc>1) {
+        service_name = argv[1];
+    } else {
+        service_name = "TEST";
+    } 
+
+    printf("SERVICE NAME: %s \r\n", service_name);
+    
     //  Create beacon to broadcast our service
     byte announcement [2] = { (port_nbr >> 8) & 0xFF, port_nbr & 0xFF };
+
+    int len = strlen(service_name)+3;
+    byte* frame = malloc(len);
+    frame[0] = announcement[0];
+    frame[1] = announcement[1];
+    strcpy((char*)&(frame[2]),service_name);
+
     zbeacon_t *service_beacon = zbeacon_new (9999);
     zbeacon_set_interval (service_beacon, 100);
-    zbeacon_publish (service_beacon, announcement, 2);
+    zbeacon_publish (service_beacon, frame, len);
 
     while (!zctx_interrupted) {
         sleep (1);
--- beacon/receiver.c
+++ beacon/receiver.c
@@ -1,10 +1,26 @@
 #include <czmq.h>
 
+int process_services(const char *key, void *item, void *argument) {
+    //  si el tiempo_actual > tiempo_guardado+3*TIMEOUT
+    //     lo elimino de la tabla
+    //     aviso de que ya no esta detectado
+
+    int* tmp = (int*)item;	
+   
+    printf("TIME %ld \r\n",*((long*)argument)); 
+    printf("KEY %s => %d\r\n", key, *tmp);
+
+    return 0;
+}
+
+long now = 0;
+
 int main(void) {
 
     //  Basic test: create a service and announce it
     zctx_t *ctx = zctx_new ();
-
+    zhash_t* services_found = zhash_new(); 
+    
     //  Create beacon to lookup service
     zbeacon_t *client_beacon = zbeacon_new (9999);
     zbeacon_subscribe (client_beacon, NULL, 0);
@@ -14,17 +30,34 @@ int main(void) {
 
     while(!zctx_interrupted) {
 
-        char *ipaddress = zstr_recv (zbeacon_pipe (client_beacon));
-        if (ipaddress) {
+        //recibo con timeout
+	char *ipaddress = zstr_recv (zbeacon_pipe (client_beacon));
+
+	//si hay trama la proceso, i.e
+	if (ipaddress) {
             zframe_t *content = zframe_recv (zbeacon_pipe (client_beacon));
             int received_port = (zframe_data (content) [0] << 8)
                                 +  zframe_data (content) [1];
+            printf("IP   DETECTED %s\r\n", ipaddress);
             printf("PORT DETECTED %d\r\n", received_port);
-            zframe_destroy (&content);
+            printf("SVC  DETECTED %s\r\n", &(((char*)content)[2]) );
+ 
+            //  si ya existe actualizo su tiempo
+	    //  sino la añado
+            int* tmp = malloc(sizeof(int));
+	    *tmp = received_port;
+	    
+	    zhash_update(services_found, ipaddress, tmp); 
+            
+	    zframe_destroy (&content);
             free (ipaddress);
-        }
+	}
 
+	//recorro todos los elementos del hash
+	now = zclock_time();
+        zhash_foreach (services_found, process_services, &now);
     }
+    
     zbeacon_destroy (&client_beacon);
     zctx_destroy (&ctx);
 
