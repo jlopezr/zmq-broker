3c2d7af1f59c475c37ffa74b9bd42c4a8d65b0ce
Juan López Rubio <j.lopez.r@gmail.com>
2013-07-22 13:13:42 -0400

    Zero copy version

--- zerocopy/zerocopy-sub.c
+++ zerocopy/zerocopy-sub.c
@@ -1,31 +1,61 @@
-//  Pathological subscriber
-//  Subscribes to one random topic and prints received messages
-
 #include "czmq.h"
+#include "zmq_utils.h"
 #include <stdlib.h>
 
 int main (int argc, char *argv [])
 {
+
+    if (argc != 4) {
+        printf ("usage: copy-sub <connect-to> <message-size> "
+                "<roundtrip-count>\n");
+        return 1;
+    }
+    char* connect_to = argv [1];
+    long message_size = atoi (argv [2]);
+    long roundtrip_count = atoi (argv [3]);
+
     zctx_t *context = zctx_new ();
     void *subscriber = zsocket_new (context, ZMQ_SUB);
-    printf("Listening on tcp://*:5556\r\n");
-    zsocket_bind (subscriber, "tcp://*:5556");
+    
+    zsocket_set_rcvhwm(subscriber , 0); 
+
+    int hwm = zsocket_rcvhwm(subscriber);
+    printf("HMW=%d\r\n", hwm);
+    
+    zsocket_connect (subscriber, connect_to);
     zsocket_set_subscribe (subscriber, "TEST");
 
-    int total_count = 1000000; 
-    int message_size = 1024;
+    int total_count = 100000; 
 
     int count = 0;
-    while (count < total_count && !zctx_interrupted) {
-	//printf("Receiving...\r\n");
+    void* watch = NULL;
+    while (count < roundtrip_count && !zctx_interrupted) {
         zmsg_t* msg = zmsg_recv(subscriber);
-        zframe_t* topic_frame = zmsg_first(msg);
+	//zmsg_dump(msg);
+
+	if(watch == NULL) {
+            watch = zmq_stopwatch_start ();
+	}
+
+	zframe_t* topic_frame = zmsg_first(msg);
         zframe_t* data_frame = zmsg_next(msg);
         assert(zframe_size(topic_frame)==4);
 	assert(zframe_size(data_frame)==message_size);
+	int i = atoi(zframe_data(data_frame));
+
+	if(i != count) {
+	    printf("EXPECTED %d - RECEIVED %d \r\n", count, i);
+	    count = i;
+	    //exit(-1);	      
+	}
+	
 	zmsg_destroy(&msg);
 	count++;
     }
+    unsigned long elapsed = zmq_stopwatch_stop (watch);
+    double latency = (double) elapsed / (roundtrip_count-1);
+    printf ("average latency: %.3f [us]\n", (double) latency);
+    
     zctx_destroy (&context);
     return 0;
 }
